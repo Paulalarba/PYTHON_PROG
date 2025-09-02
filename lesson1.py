1efb375534bd6a91c7fe4cbc195874e908daf66d
PAUL ALARBA <alarbapaulax@gmail.com>
2025-09-02 11:28:35 +0800

    print

---
+++ lesson1.py
@@ -0,0 +1,10 @@
+"""reverse words
+    false class return is finally
+    None if for lambda continue
+    true def from while nonlocal
+    and del global not with 
+    as elif try of yield
+    assert else import pass
+    break except in raise
+"""
+print('hello world')
