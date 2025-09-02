7e9db9b14e6cafcc48feb7c43b4adec30798f043
PAUL ALARBA <alarbapaulax@gmail.com>
2025-09-02 11:39:45 +0800

    virables

---
+++ Variables.py
@@ -0,0 +1,13 @@
+"""reverse words
+    false class return is finally
+    None if for lambda continue
+    true def from while nonlocal
+    and del global not with 
+    as elif try of yield
+    assert else import pass
+    break except in raise
+"""
+# constants : fixed values such as numbers letters and strings
+width = 15
+height = 12.0
+print(height/3)
