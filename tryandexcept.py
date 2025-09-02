a79e25a8627374b9a299b3df092903d32e4b9102
PAUL ALARBA <alarbapaulax@gmail.com>
2025-09-02 12:30:34 +0800

    try and except

---
+++ tryandexcept.py
@@ -0,0 +1,49 @@
+"""reverse words
+    false class return is finally
+    None if for lambda continue
+    true def from while nonlocal
+    and del global not with 
+    as elif try of yield
+    assert else import pass
+    break except in raise
+"""
+# constants : fixed values such as numbers letters and strings
+# comparison operators :
+# multi-way
+"""x = 1
+if x < 2:
+    print('small')
+elif x < 10:
+    print('medium')
+else:
+    print('large')
+print('all done')"""
+# try except if the try works the except is skipped: if the try fails it jumps to the except
+"""astr = 'paul'
+try:
+    num = int(astr)
+except:
+    num = 20
+print('Paul', num)
+age = 20
+try:
+    name = int(age)
+except:
+    name = 'paul'
+print('paul', name)"""
+name = 'paul'
+try:
+    surname = int(name)
+except:
+    surname = 'Alarba'
+print('hi', surname)
+
+
+astr = 'bob'
+try:
+    print('hello')
+    istr = int(astr)
+    print('there')
+except:
+    istr = -1
+print('Done', istr)
