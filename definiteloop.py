c3437659e4280b7127bf27b95901894b0ec2e12d
PAUL ALARBA <alarbapaulax@gmail.com>
2025-09-08 12:19:02 +0800

    definite loops

---
+++ definiteloop.py
@@ -0,0 +1,9 @@
+for i in [5, 4, 3, 2, 1]:
+    print(i)
+print('Blastoff')
+
+# definite loop with strings
+friends = ['angelo', 'xmen', 'rodulfo']
+for friend in friends:
+    print(f"HAPPY NEW YEAR {friend}")
+print('Done!')
