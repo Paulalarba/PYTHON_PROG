82ecf32b2c398eccb45a0d764aaf78843a2a6263
PAUL ALARBA <alarbapaulax@gmail.com>
2025-09-10 00:30:49 +0800

    string basic

---
+++ string.py
@@ -0,0 +1,25 @@
+"""String = 'Paul Alarba'
+name = String[0]
+print(name)
+
+first_name = "Paul"
+n = 0
+fname = first_name[n - 2]
+print(fname)
+
+while n < len(String):
+    lenString = String[n]
+    print(f"{n} {lenString}")
+    n = n + 1
+
+for x in String:
+    print(x)
+
+caunt = 0
+for last_name in String:
+    if last_name == 'a':
+        caunt = caunt + 1
+print(caunt)"""
+
+for n in "banana":
+    print(n)
