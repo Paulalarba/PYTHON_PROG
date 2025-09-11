9fcc1f48b45d402d52c96ded60fc30ad2274d81b
PAUL ALARBA <alarbapaulax@gmail.com>
2025-09-12 01:02:55 +0800

    fucking string again kay 10 ra akong kuha sa exam1

--- string.py
+++ string.py
@@ -1,25 +1,29 @@
-"""String = 'Paul Alarba'
-name = String[0]
+# multipke sting assignment assignment
+
+name4, name5, name6 = "rhea", "cris alvy", "janna"
+name1, name2, name3 = "Paul", "angelo", "xmen"
+
+name6, name2, name3 = name1, name4, name5
+
+print(name1)
+print(name2)
+
+#Dynamic typing in python it means you can re assign the variable as many as you want example:
+
+name = "Paul"
+name = 8
+name = ["Paul", "Alarba"]
+# the output of the code should be the list
 print(name)
+# but the dynamic typing is not advisable because it is unpradictable
 
-first_name = "Paul"
-n = 0
-fname = first_name[n - 2]
-print(fname)
+# type casting a variable in python can be converted using type castting
 
-while n < len(String):
-    lenString = String[n]
-    print(f"{n} {lenString}")
-    n = n + 1
+age = 20.2
+age = int(age)
+print(age)
 
-for x in String:
-    print(x)
+first_name = "12.12"
+first_name = int(first_name)
+print(first_name) #this is not valid type casting since you cant convert a string to number
 
-caunt = 0
-for last_name in String:
-    if last_name == 'a':
-        caunt = caunt + 1
-print(caunt)"""
-
-for n in "banana":
-    print(n)
