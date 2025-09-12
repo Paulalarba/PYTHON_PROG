e4e2d5ccdf22cf64b26899f4916fee80e4583978
PAUL ALARBA <alarbapaulax@gmail.com>
2025-09-12 23:39:21 +0800

    math nasad kay na 10/50 ko sa exam

---
+++ math_1.py
@@ -0,0 +1,27 @@
+# the general rule of math is PMDAS always remember from left to right
+
+
+a, b, c, d = 12, 32.1, 21, 1
+
+sum = a + b + c + d
+product = a * b * c * d
+quotient = a / b / c / d
+difference = a - b - c - d
+
+print(sum)
+print(product)
+print(quotient)
+print(difference)
+
+x, y = 5, 2
+# floar division: // divides the first operand by the seconde operand and rounds down the result to an intger
+print(x // y) # since 5/2 is 2.5 and when you round down the result we get is 2
+
+# modulus %: return the remainder of the division of the first operand by the second
+print(x % y)
+
+# shorthand operator in python
+count = 0
+count = count + 1 # is just same 
+count += count
+print(count)
