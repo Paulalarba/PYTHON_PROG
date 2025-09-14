414e9a50e18b480bdc65d29c9ffaf886f7255538
PAUL ALARBA <alarbapaulax@gmail.com>
2025-09-15 02:30:25 +0800

    strings again

--- Strings.py
+++ Strings.py
@@ -2,7 +2,7 @@
 # implement a function called get_longer_word(word1, word2) it takes two words strins as parameters
 # and it returns the longer word, if the words are the same lenght return the same length
 
-"""def get_longer_word(word1, word2):
+def get_longer_word(word1, word2):
     word1 = len(input("Enter your first word: "))
     word2 = len(input("Enter your second word: "))
 
@@ -26,13 +26,13 @@ def first_n_charcters():
     s = input("Enter the name you want: ")
     n = int(input("Enter the len of the string: "))
     print(s[n:])
-first_n_charcters()"""
+first_n_charcters()
 
 # this is how you reverse the string
 # note if you want to reverse the string the stop should be greater than the start
 def string_reverse():
     string = input("Enter the string: ")
-    n = int(input("Enter the start "))
-    j = int(input("Enter the stop"))
+    n = int(input("Enter the start: "))
+    j = int(input("Enter the stop: "))
     print(string[n:j:-1])
 string_reverse()
